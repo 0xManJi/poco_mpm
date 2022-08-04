c7d8d8fba3dc3b2e3e86eedd1c5f5facc2d259e5
0xManJi <cdkk5555@qq.com>
2022-08-04 23:34:49 +0800

    poco+air

--- case/poco_case.py
+++ case/poco_case.py
@@ -5,15 +5,18 @@ __author__ = "ZHANG CHUN HE"
 
 from airtest.core.api import *
 
-auto_setup(__file__,devices=["android://127.0.0.1:5037/HLRDU19807001907"],logdir=True)
+auto_setup(__file__,devices=["android://127.0.0.1:5037/127.0.0.1:7555"],logdir=True)
 
 from poco.drivers.android.uiautomation import AndroidUiautomationPoco
 poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
 
 
-start_app("com.tencent.mm")
+poco(text="微信").click()
 swipe((550,250),(550,1200))
 sleep(2.0)
-poco("com.tencent.mm:id/f15").click()
-
+poco("com.tencent.mm:id/f1c").click()
+sleep(2.0)
 poco(text="搜索商品/运动/款号").click()
+
+wait(Template(r"../images/tpl1659626999336.png", record_pos=(-0.091, -0.779), resolution=(900, 1600)))
+text("饮料")
